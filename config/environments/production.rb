107df64c079953465122eeac0838d4d3ca813728
Tanaka <mixed_fruits_6o6_uxu@yahoo.co.jp>
2018-01-20 10:01:46 +0000

    third

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   # use sendgrid
-  config.action_mailer.default_url_options = { host: 'agile-mesa-41313.herokuapp.com/contacts'}
+  config.action_mailer.default_url_options = { host: 'agile-mesa-41313.herokuapp.com/'}
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = 
   {
